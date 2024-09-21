dfc88f912ea17c650f9623f3b8457d3af958c640
HoussamCbk <choubikhoussam@gmail.com>
2024-09-21 14:09:10 +0100

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,172 @@
+import logoensam from '../../src/images/Education/logoensam.png';
+
+export const Bio = {
+  name: "",
+  roles: [
+    "Choubik Houssam",
+  ],
+  description:
+    "an engineering student at ENSAM Casablanca",
+  github: "https://github.com/HoussamCbk",
+  resume:
+    "",
+  linkedin: "https://www.linkedin.com/in/houssam-choubik-5b4ba3316/",
+  github: "https://github.com/HoussamCbk",
+  insta: "https://www.instagram.com/houssam_clap/",
+  facebook: "https://web.facebook.com/houssam.clap/",
+  email: "mailto:choubikhoussam@gmail.com",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Mechanical Design",
+    skills: [
+      {
+        name: "Catia V5",
+        image:
+          "https://www.logolynx.com/images/logolynx/0b/0b2a9114752330b0719048c346e51886.jpeg",
+      },
+      {
+        name: "Fusion 360",
+        image: "https://i.pinimg.com/1200x/79/c1/4a/79c14a06b3a0141a7709d66435234c4b.jpg",
+      },
+    ],
+  },
+  {
+    title: "Laser cut Design",
+    skills: [
+      {
+        name: "Inkscape",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/a/a2/Inkscape_logo_%282-colour%29.svg",
+      },
+    ],
+  },
+
+  {
+    title: "Design",
+    skills: [
+      {
+        name: "Canva",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQZR2p-_G2EcQbIxqw6_VH9Imua_NiwRPEjOQ&s",
+      },
+    ],
+  },
+  {
+    title: "Mechatronics",
+    skills: [
+      {
+        name: "Arduino IDE",
+        image:
+          "https://content.arduino.cc/assets/arduino_logo_1200x630-01.png",
+      },
+      {
+        name: "Blynk Iot",
+        image:
+          "https://www.researchgate.net/profile/Shehab-Noor/publication/345431623/figure/fig49/AS:955186143039504@1604745531967/Official-Blynk-logo_Q320.jpg",
+      },
+      {
+        name: "Arduino Cloud",
+        image:
+          "https://user-images.githubusercontent.com/77038120/174824422-29e21c5a-0a78-4424-ac6a-cf206aeb0d26.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeeaLK0GLqYtre8j8ScXOO4v5iMo8e4Vls2Q&s",
+    role: "Logistic Manager",
+    company: "Arts & Crafts Aeronautics | Club at ENSAMc",
+    date: "August 2024 - Today",
+    desc: "This role involves managing resources, transportation, and event setups to ensure that operations run smoothly during meetings, workshops, and events.",
+    skills: [
+      "Organisation",
+      "Group Work",
+      "Managing resources",
+      
+    ],
+    doc: "",
+  },
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRza2BVcnSEbpQKKp93kzvsaMK6U6LRQHthiQ&s",
+    role: "Member of the logistic comity",
+    company: "A&M Mechatronics | Club at ENSAMc",
+    date: "August 2024 - Today",
+    desc: "This role involves managing resources, transportation, and event setups to ensure that operations run smoothly during meetings, workshops, and events.",
+    skills: [
+      "Organisation",
+      "Group Work",
+      "Managing resources",
+      
+    ],
+    doc: "",
+  },
+  
+];
+
+export const education = [
+  {
+    id: 0,
+    img: logoensam,
+    school: "College : ENSAM Casablanca",
+    date: "2023 -  2028",
+    desc: "I am currently continuing my engineering studies at ENSAM Casablanca. I am in the second year of the preparatory cycle.",
+    degree: "Engineering",
+  },
+  
+ 
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "DecisionHub",
+    date: "Jan 2024 - Dec 2023",
+    description:
+      "A Rule Builder application “Decision Hub” that empowers Business Analysts to create, save, and visualize decision strategies. Provide a no-code rule writing experience and visual representation to test these rules in real-time and observe the calculations at each step.",
+    image:
+      "https://github.com/rishavchanda/DecisionHub/raw/master/assets/testRule.jpg",
+    tags: [
+      "React Js",
+      "PostgressSQL",
+      "Node Js",
+      "Express Js",
+      "Redux",
+      "React Flow",
+    ],
+    category: "web app",
+    github: "https://github.com/rishavchanda/DecisionHub",
+    webapp: "https://decisionhub.netlify.app/",
+  },
+  
+];
+
